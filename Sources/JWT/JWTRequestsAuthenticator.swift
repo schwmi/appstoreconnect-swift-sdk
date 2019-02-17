4d3c25ad98ae1dccb5980852f5c21f4e065de15a
Michael Schwarz <michael_schwarz@me.com>
2019-02-17 20:09:12 +0100

    Use openssl based JWT library

--- Sources/JWT/JWTRequestsAuthenticator.swift
+++ Sources/JWT/JWTRequestsAuthenticator.swift
@@ -6,38 +6,51 @@
 //
 
 import Foundation
+import JWT
 
 /// An Authenticator for URL Requests which makes use of the RequestAdapter from Alamofire.
 final class JWTRequestsAuthenticator {
 
-    private var cachedToken: JWT.Token?
+    private var cachedToken: JWT?
     private let apiConfiguration: APIConfiguration
 
     /// The JWT Creator to use for creating the JWT token. Can be overriden for test use cases.
-    var jwtCreator: JWTCreatable
+    let signer: ES256Signer
 
     init(apiConfiguration: APIConfiguration) {
+        let es256Key = ES256Key(pemString: apiConfiguration.privateKey)
+        self.signer = ES256Signer(privateKey: es256Key)
         self.apiConfiguration = apiConfiguration
-        self.jwtCreator = JWT(keyIdentifier: apiConfiguration.privateKeyID, issuerIdentifier: apiConfiguration.issuerID, expireDuration: 60 * 20)
     }
 
     /// Generates a new JWT Token, but only if the in memory cached one is not expired.
-    private func createToken() throws -> JWT.Token {
-        if let cachedToken = cachedToken, !cachedToken.isExpired {
-            return cachedToken
+    private func createToken() throws -> String {
+        if let cachedToken = self.cachedToken, !cachedToken.isExpired {
+            return try cachedToken.makeToken()
         }
 
-        let token = try jwtCreator.signedToken(using: apiConfiguration.privateKey)
-        cachedToken = token
-        return token
+        let jwt = JWT(header: .init(keyIdentifier: self.apiConfiguration.privateKeyID),
+            payload: .init(issuerIdentifier: self.apiConfiguration.issuerID,
+                           expirationTime: Date().timeIntervalSince1970 + 60 * 15),
+            signer: self.signer)
+        return try jwt.makeToken()
     }
 }
 
 extension JWTRequestsAuthenticator {
+
     func adapt(_ urlRequest: URLRequest) throws -> URLRequest {
         let token = try createToken()
         var urlRequest = urlRequest
         urlRequest.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
         return urlRequest
+    }
+}
+
+extension JWT {
+
+    var isExpired: Bool {
+        // TODO: Implement
+        return true
     }
 }
