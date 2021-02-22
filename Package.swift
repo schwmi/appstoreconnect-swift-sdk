ff0c4c456e370941db9b2680a647d4a8a0ee2e1c
Michael Schwarz <michael_schwarz@me.com>
2021-02-22 23:45:52 +0100

    Update package.swift

--- Package.swift
+++ Package.swift
@@ -1,16 +1,19 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
     name: "AppStoreConnect-Swift-SDK",
+    platforms: [
+       .macOS(.v10_15)
+    ],
     products: [
         .library(
             name: "AppStoreConnect-Swift-SDK",
             targets: ["AppStoreConnect-Swift-SDK"])
     ],
     dependencies: [
-        .package(url: "https://github.com/schwmi/JWT.git", .upToNextMajor(from: "0.1.0"))
+        .package(url: "https://github.com/schwmi/JWT.git", .upToNextMajor(from: "0.2.0"))
         ],
     targets: [
         .target(
