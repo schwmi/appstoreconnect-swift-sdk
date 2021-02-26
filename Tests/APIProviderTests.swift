8e704a2d48a3d45eb38735417e9b0bc0134b81ed
Michael Schwarz <michael_schwarz@me.com>
2021-02-26 22:45:50 +0100

    Fix timezone test

--- Tests/APIProviderTests.swift
+++ Tests/APIProviderTests.swift
@@ -179,6 +179,7 @@ final class APIProviderTests: XCTestCase {
         let outputFormatter = DateFormatter()
         outputFormatter.dateFormat = "E, d MMM yyyy HH:mm:ss Z"
         outputFormatter.locale = Locale(identifier: "en_US_POSIX")
+        outputFormatter.timeZone = TimeZone(identifier: "GMT")!
 
         let dateFrom: (String) throws -> String = { dateString in
             let date = try decoder.decode(Date.self, from: "\"\(dateString)\"".data(using: .utf8)!)
