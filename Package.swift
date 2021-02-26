5ce193e5acf50204c9b1dbe5e26ef11e5d077a35
Michael Schwarz <michael_schwarz@me.com>
2021-02-26 22:43:28 +0100

    Add unit tests to spm

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version:5.1
-// We're hiding dev, test, and danger dependencies with // dev to make sure they're not fetched by users of this package.
+// swift-tools-version:5.3.0
 
 import PackageDescription
 
@@ -10,16 +9,15 @@ let package = Package(
         .macOS(.v10_12)
     ],
     products: [
-        // dev .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]),
         .library(name: "AppStoreConnect-Swift-SDK", targets: ["AppStoreConnect-Swift-SDK"])
     ],
-    dependencies: [
-        // dev .package(url: "https://github.com/danger/swift", from: "3.0.0"),
-        // dev .package(path: "Submodules/WeTransfer-iOS-CI/Danger-Swift")
-        ],
+    dependencies: [],
     targets: [
-        // dev .target(name: "DangerDependencies", dependencies: ["Danger", "WeTransferPRLinter"], path: "Submodules/WeTransfer-iOS-CI/Danger-Swift", sources: ["DangerFakeSource.swift"]),
-        // dev .testTarget(name: "AppStoreConnect-Swift-SDK-Tests", dependencies: ["AppStoreConnect-Swift-SDK"], path: "Tests", exclude: ["LinuxMain.swift"]),
+        .testTarget(name: "AppStoreConnect-Swift-SDK-Tests",
+                    dependencies: ["AppStoreConnect-Swift-SDK"],
+                    path: "Tests",
+                    exclude: ["LinuxMain.swift"],
+                    resources: [.process("Models/Fixtures.bundle")]),
         .target(name: "AppStoreConnect-Swift-SDK", path: "Sources")
     ]
 )
